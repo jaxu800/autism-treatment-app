ed3b516f713f72d59fad616882fbeda71a4eb1fd
Cora Selzer <cselzer@asu.edu>
2021-03-10 18:51:20 -0700

    Added tabs and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,13 @@ import {
   IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { alertCircleOutline, homeOutline, bandageOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import AnrcGuide from './pages/AnrcGuide';
+import Assessments from './pages/Assessments';
+import Add from './pages/Add';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -44,8 +47,17 @@ const App: React.FC = () => (
           <Route exact path="/tab2">
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route exact path="/tab3">
             <Tab3 />
+          </Route>
+          <Route exact path="/anrc">
+            <AnrcGuide />
+          </Route>
+          <Route exact path="/assmt">
+            <Assessments />
+          </Route>
+          <Route exact path="/add">
+            <Add />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
@@ -53,16 +65,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={homeOutline} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={bandageOutline} />
+            <IonLabel>Treatments</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon icon={alertCircleOutline} />
+            <IonLabel>Symptoms</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
