3e99c2f2445349eebc2e2ea569bbcfb983216eca
Moses-kang <66771008+Moses-kang@users.noreply.github.com>
2021-04-07 20:05:56 -0700

    Added Dynamic Paging

--- src/components/TreatmentList.tsx
+++ src/components/TreatmentList.tsx
@@ -3,27 +3,27 @@ import { IonItem, IonLabel, IonItemGroup, IonRouterOutlet  } from '@ionic/react'
 const TreatmentList: React.FC = () => {
     return (  
         <IonItemGroup>
-            <IonItem button href="/treatments/treatment1" onClick={() => { }} detail>
+            <IonItem button href="/treatments/treatment/1" onClick={() => { }} detail>
                 <IonLabel>
                     Treatment 1
                 </IonLabel>
             </IonItem>
-            <IonItem button href="/treatments/treatment2" onClick={() => { }} detail>
+            <IonItem button href="/treatments/treatment/2" onClick={() => { }} detail>
                 <IonLabel>
                     Treatment 2
                 </IonLabel>
             </IonItem>
-            <IonItem button href="/treatments/treatment3" onClick={() => { }} detail>
+            <IonItem button href="/treatments/treatment/3" onClick={() => { }} detail>
                 <IonLabel>
                     Treatment 3
                 </IonLabel>
             </IonItem>
-            <IonItem button href="/treatments/treatment4" onClick={() => { }} detail>
+            <IonItem button href="/treatments/treatment/4" onClick={() => { }} detail>
                 <IonLabel>
                     Treatment 4
                 </IonLabel>
             </IonItem>
-            <IonItem button href="/treatments/treatment5" onClick={() => { }} detail>
+            <IonItem button href="/treatments/treatment/5" onClick={() => { }} detail>
                 <IonLabel>
                     Treatment 5
                 </IonLabel>
