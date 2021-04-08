ff65bef4b335ff2065d30b8d8759e08799f14719
Moses-kang <66771008+Moses-kang@users.noreply.github.com>
2021-04-07 20:04:57 -0700

    Added dynamic paging

--- src/pages/TreatmentPage.tsx
+++ src/pages/TreatmentPage.tsx
@@ -1,19 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonBackButton, IonButtons } from '@ionic/react';
+import { RouteComponentProps } from 'react-router';
 import TreatmentContainer from '../components/TreatmentContainer';
+import './Tab2.css';
 
-const TreatmentPage: React.FC = () => {
+
+interface treatmentDetailsProps extends RouteComponentProps<{
+  temp: string;
+}> {}
+
+const TreatmentPage: React.FC<treatmentDetailsProps> = ({match, history}) => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Treatment X</IonTitle>
+          <IonTitle>Treatment {match.params.temp}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Treatment X</IonTitle>
+            <IonTitle size="large">Treatment</IonTitle>
           </IonToolbar>
+
         </IonHeader>
         <TreatmentContainer />
       </IonContent>
