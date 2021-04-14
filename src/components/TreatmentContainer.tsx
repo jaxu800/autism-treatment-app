dce6159108faf88e76c39a95caa6d239f19b73cf
Cora Selzer <cselzer@asu.edu>
2021-04-13 17:48:26 -0700

    header changes

---
+++ src/components/TreatmentContainer.tsx
@@ -0,0 +1,13 @@
+import './SymptomContainer.css';
+
+
+const SymptomContainer: React.FC = () => {
+  return (
+    <div className="symptomcontainer">
+      <strong>Symptom Page</strong>
+      <p>Graph Goes Here</p>
+    </div>
+  );
+};
+
+export default SymptomContainer;
