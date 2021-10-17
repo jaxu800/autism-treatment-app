479d9f343337d7dd7d1f3d6053c93f3738d01f0a
Cora Selzer <coraselzer@gmail.com>
2021-10-17 16:38:51 -0700

    UI updates

--- src/pages/SymptomPage.tsx
+++ src/pages/SymptomPage.tsx
@@ -12,8 +12,8 @@ const SymptomPage: React.FC<symptomsDetailsProps> = ({match, history}) => {
     <IonPage>
       <IonHeader>
         <IonToolbar className="new-background-color">
-          <IonButtons  slot="start">
-	    <IonBackButton  style={{height: 'var(--min-height)', width: 'var(--min-width)', color: 'var(--light-blue-1)', display: 'block'}} />
+          <IonButtons slot="start">
+	    <IonBackButton style={{height: 'var(--min-height)', width: 'var(--min-width)', color: 'var(--light-blue-1)', display: 'block'}} />
           </IonButtons>
 
 	  <IonTitle style={{padding: '8px', color: 'var(--light-blue-1)', textAlign: "left", fontSize: 16, height: '65px'}}>Symptom: {match.params.temp}</IonTitle>
--- src/pages/Symptoms.tsx
+++ src/pages/Symptoms.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton, IonMenu, IonSelect, IonSelectOption, IonList, IonLabel, IonItem } from '@ionic/react';
 import SymptomList from '../components/SymptomList';
 import './Tab3.css';
 
@@ -8,11 +8,40 @@ const Symptoms: React.FC = () => {
       <IonHeader>
         <IonToolbar className="new-background-color">
           <IonButtons  slot="start">
-			<IonBackButton  style={{height: 'var(--min-height)', width: 'var(--min-width)', color: 'var(--light-blue-1)', display: 'block'}} />
+	          <IonBackButton style={{height: 'var(--min-height)', width: 'var(--min-width)', color: 'var(--light-blue-1)', display: 'block'}} />
           </IonButtons>
-		  <IonTitle style={{padding: '2px', color: 'var(--light-blue-1)', textAlign: "left", fontSize: 36, height: '65px'}}>Symptoms</IonTitle>
+	        <IonTitle style={{padding: '2px', color: 'var(--light-blue-1)', textAlign: "left", fontSize: 24, height: '65px'}}>Symptoms</IonTitle>
         </IonToolbar>
       </IonHeader>
+
+      <IonMenu side="start" menuId="first">
+        <IonHeader>
+          <IonToolbar className="new-background-color">
+            <IonTitle>Filter Options</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonList>
+            <IonItem>
+              <IonLabel>Treatment Type</IonLabel>
+              <IonSelect value="all">
+                <IonSelectOption value="all">All</IonSelectOption>
+                <IonSelectOption value="pharmaceuticals">Pharmaceuticals</IonSelectOption>
+                <IonSelectOption value="nutraceuticals">Nutraceuticals</IonSelectOption>
+                <IonSelectOption value="therapies">Therapies</IonSelectOption>
+                <IonSelectOption value="diets">Diets</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Sort By</IonLabel>
+              <IonSelect value="overall">
+                <IonSelectOption value="overall">Best Overall (include negative effects)</IonSelectOption>
+                <IonSelectOption value="benefit">Biggest Benefit (ignore negative effects)</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
 
       <IonContent fullscreen className="new-background-color-2">
         <IonHeader collapse="condense">
