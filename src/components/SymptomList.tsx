3b99c5a78f33e88702c29a583a7b57452308f6c6
Moses Kang <mkang41@asu.edu>
2021-12-02 11:33:02 -0700

    moses changes

--- src/components/SymptomList.tsx
+++ src/components/SymptomList.tsx
@@ -1,22 +1,48 @@
 import { IonItem, IonLabel, IonItemGroup } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, get, child } from 'firebase/database'
+
 
 const SymptomList: React.FC = () => {
-    var symptomss = ["Aggression", "Anxiety", "Attention", "Cognition", "Communication", "Irritability" ];
-    return (
-        <IonItemGroup>
-        {
-            symptomss.map(elem => {
-                return(
-                    <IonItem className="new-background-color-2" button href={"symptoms/" + elem} onClick={() => {}} detail>
-                        <IonLabel>
-                            {elem}
-                        </IonLabel>
-                    </IonItem>
-                )
-            })
-        }
-        </IonItemGroup>
-    );
-  };
+
+  const initArray: string[] = []
+  const [symptoms, setSyptoms] = useState(initArray);
+  const loadData = () => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `symptoms`)).then((snapshot) => {
+      var symptomsList:string[]  = [];
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          symptomsList.push(String(childSnapshot.key));
+        });
+      } else {
+        console.log("No data available");
+      }
+      setSyptoms(symptomsList);
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  useEffect(() => {
+    loadData()
+  }, []);
+
+  return (
+      <IonItemGroup>
+      {
+          symptoms.map(elem => {
+              return(
+                  <IonItem className="new-background-color-2" button href={"symptoms/" + elem} onClick={() => {}} detail>
+                      <IonLabel>
+                          {elem}
+                      </IonLabel>
+                  </IonItem>
+              )
+          })
+      }
+      </IonItemGroup>
+  );
+};
   
   export default SymptomList;
--- src/pages/SymptomPage.tsx
+++ src/pages/SymptomPage.tsx
@@ -1,115 +1,177 @@
-import { IonContent, 
-        IonHeader, 
-        IonPage, 
-        IonTitle, 
-        IonToolbar, 
-        IonItem, 
-        IonLabel, 
-        IonButtons, 
-        IonBackButton, 
-        IonItemGroup } from '@ionic/react';
-import { RouteComponentProps } from 'react-router';
-import  { HorizontalBar, Bar } from 'react-chartjs-2';
-import './SymptomContainer.css';
+import {
+  IonContent, IonHeader,
+  IonPage, IonTitle,
+  IonToolbar, IonItem,
+  IonLabel, IonButtons,
+  IonBackButton, IonItemGroup,
+} from "@ionic/react";
+import { RouteComponentProps } from "react-router";
+import Chart, { Bar } from "react-chartjs-2";
+import "./SymptomContainer.css";
+import React, { useEffect, useState, MouseEvent } from 'react';
+import { getDatabase, ref, onValue, get, child, } from 'firebase/database'
 
-import './Tab3.css';
+interface symptomsDetailsProps
+  extends RouteComponentProps<{
+    temp: string;
+  }> {}
 
-interface symptomsDetailsProps extends RouteComponentProps<{
-  temp: string;
-}> {}
+const SymptomPage: React.FC<symptomsDetailsProps> = ({ match, history }) => {
+  const[treatmentss,setTreatments] = useState({})
+  
 
-const SymptomPage: React.FC<symptomsDetailsProps> = ({match, history}) => {
-  var treatments = ["5-HTP", "Biotin", "Blend of Amino Acids", "Calcium", "Carntine", "Chromium", "CoQ10", "Cod Liver Oil", "DMG", "Glutamine", "Iron", "Iodine", "Melatonin", "Omega 3", "Potassium", "Taurine", "Vitamin D", "Zinc"];
+  const loadData = () => {
+    var dbref = ref(getDatabase())
+    get(child(dbref, `symptoms/${match.params.temp}`)).then((snapshot) => {
+      var temp:Object = 0;
+      if (snapshot.exists()){
+        temp = snapshot.toJSON()!
+      } else {
+        console.log("No data available");
+      }
+      setTreatments(temp)
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
 
-  const repeatedArray = [].concat(...Array(20).fill([
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-  ],));
+  useEffect(() => {
+    loadData()
+  }, []);
 
-  const treatementsRating = {
-    labels: ['Melatonin', '5-HTP', 'Tryptophan', 'Valerian Root','Melatonin', '5-HTP', 'Tryptophan', 'Valerian Root',],
+  var treatments = Object.keys(treatmentss);
+
+  const handleMouseEvent = (e: MouseEvent<HTMLElement>) => {
+
+  }
+
+  const repeatedArray = [].concat(
+    ...Array(20).fill([
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+    ])
+  );
+
+  const treatmentsRating = {
+    labels: treatments,
     datasets: [
       {
-        label: 'Overall Net Benefit Rating',
+        label: "Overall Net Benefit Rating",
         backgroundColor: repeatedArray,
         borderColor: repeatedArray,
         borderWidth: 2,
-        data: [0.63, 0.46, 0.32, 0.24,0.63, 0.46, 0.32, 0.24]
-      }
-    ]
+        data: Object.values(treatmentss),
+      },
+    ],
   };
 
   const treatmentOptions = {
     title: {
       display: true,
       fontSize: 15,
-      text: `Top Treatments for ${match.params.temp}`,
+      text: `Top Rated Treatments`,
     },
     legend: {
       display: false,
     },
     maintainAspectRatio: false,
     scales: {
-      xAxes: [{
-        scaleLabel: {
-          display: false, 
+      xAxes: [
+        {
+          display:false,
+          overflow:true,
+          scaleLabel: {
+            display: false,
+          },
+          ticks: {
+            min: 0,
+            max: 1,
+            callback: function (value: any) {
+              return value;
+            },
+          },
         },
-        ticks: {
-          min: 0,
-          max: 1,
-          callback: function(value:any) {
-            return value
-          }
-        }
-      }],
-      yAxes: [{
-        scaleLabel: {
+      ],
+      yAxes: [
+        {
+          overflow: true,
           display: true,
-          labelString: 'Overall Net Benefit Rating'
-        }
-      }]
-    }
+            ticks: {
+                suggestedMin: 0,
+                beginAtZero: true 
+            },
+        },
+      ],
+    },
   };
-  
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar className="new-background-color">
           <IonButtons slot="start">
-	          <IonBackButton defaultHref="/symptoms" style={{height: 'var(--min-height)', width: 'var(--min-width)', color: 'var(--light-blue-1)', display: 'block'}} />
+            <IonBackButton
+              defaultHref="/symptoms"
+              style={{
+                height: "var(--min-height)",
+                width: "var(--min-width)",
+                color: "var(--light-blue-1)",
+                display: "block",
+              }}
+            />
           </IonButtons>
-	        <IonTitle style={{padding: '8px', color: 'var(--light-blue-1)', textAlign: "left", fontSize: 22, height: '65px'}}>Symptom Details</IonTitle>
+          <IonTitle
+            style={{
+              padding: "8px",
+              color: "var(--light-blue-1)",
+              textAlign: "left",
+              fontSize: 22,
+              height: "65px",
+            }}
+          >
+            Symptom Details
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen className="new-background-color-2">
         <>
-        <div className="starDiv" style={{padding: '5px'}}>
-			    <h1 style={{color: 'black', fontWeight: 'bold', fontFamily: 'sans-serif'}}>{match.params.temp}</h1>
-          <div className="container">
-            <Bar data={treatementsRating} options={treatmentOptions} ></Bar>
+          <div className="starDiv" style={{ padding: "5px" }}>
+            <h1
+              style={{
+                color: "black",
+                fontWeight: "bold",
+                fontFamily: "sans-serif",
+              }}
+            >
+              {match.params.temp}
+            </h1>
+            <div className="graphExpander">
+              <Bar data={treatmentsRating} options={treatmentOptions}></Bar>
+            </div>
           </div>
-        </div>
 
-          <IonTitle style={{padding: '10px', fontSize: 18, fontWeight: 'bold'}}>
-            Recomended Treatments
+          <IonTitle style={{ padding: "10px", fontSize: "16" }}>
+            Top Rated Treatments
           </IonTitle>
 
           <IonItemGroup>
-          {
-          treatments.map(elem => {
-            return(
-              <IonItem className="new-background-color-2" button href={"treatments/" + elem} onClick={() => {}} detail>
-                <IonLabel>
-                  {elem}
-                </IonLabel>
-              </IonItem>
-            )
-          })
-          }
+            {treatments.map((elem) => {
+              return (
+                <IonItem
+                  className="new-background-color-3"
+                  button
+                  href={"treatments/" + elem}
+                  onClick={() => {}}
+                  detail
+                >
+                  <IonLabel>{elem}</IonLabel>
+                </IonItem>
+              );
+            })}
           </IonItemGroup>
         </>
       </IonContent>
